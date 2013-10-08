7f701b208014cae284a787c34421ebdbd3a31da9
toan <toan@macmini.cty>
2013-10-08 09:30:03 +0700

    Add NewsReaderViewController

---
+++ VnexpressRssFeed/NewsReaderViewController.h
@@ -0,0 +1,13 @@
+//
+//  NewsReaderViewController.h
+//  VnexpressRssFeed
+//
+//  Created by Coder on 10/8/13.
+//  Copyright (c) 2013 iOSTraining. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NewsReaderViewController : UIViewController
+
+@end
---
+++ VnexpressRssFeed/NewsReaderViewController.m
@@ -0,0 +1,38 @@
+//
+//  NewsReaderViewController.m
+//  VnexpressRssFeed
+//
+//  Created by Coder on 10/8/13.
+//  Copyright (c) 2013 iOSTraining. All rights reserved.
+//
+
+#import "NewsReaderViewController.h"
+
+@interface NewsReaderViewController ()
+
+@end
+
+@implementation NewsReaderViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
